a202f570a8e0bd3beb942289904665163a7a662a
Victor Marques <victyrmarques@gmail.com>
2021-11-23 20:16:54 -0300

    inclusão de imagem na barra/header da aplicação

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,14 @@
         app
         color="dark_blue"
         >
+        <v-avatar :tile="true">
+            <img :src="require('@/assets/logo.png')" alt="logo">
+        </v-avatar>
         <v-toolbar-title class="white--text title">Segurindo analisador</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
-      <router-view></router-view>
-      
+        <router-view></router-view>
     </v-main>
     
     <VLibras />
@@ -19,9 +21,12 @@
 
 <script>
 import { VLibras } from '@vue-a11y/vlibras'
+
 export default {
     name: 'App',
-    components:{VLibras},
+    components:{
+        VLibras
+    },
     data: () => ({
     //
     }),
