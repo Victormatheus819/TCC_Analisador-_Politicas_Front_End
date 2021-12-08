e208d6f969d336143932cfae7fa1a3fd143fd578
Victor Marques <victyrmarques@gmail.com>
2021-12-08 19:21:50 -0300

    ajuste url

--- src/components/config/Http.js
+++ src/components/config/Http.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-	baseURL: "http://127.0.0.1:8000/",//https://tcc-analise-poli-priv.herokuapp.com/",
+	baseURL: "https://tcc-analise-poli-priv.herokuapp.com/",
 	timeout: 100000
 });
 
